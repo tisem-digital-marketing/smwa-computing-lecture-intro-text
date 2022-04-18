692c5979ad08cbba0b36842bcc8ea84ebad982f8
Lachlan Deer <lachlan.deer@gmail.com>
2022-04-18 22:26:43 +0200

    template R script for students

---
+++ intro_text.R
@@ -0,0 +1,39 @@
+#' intro_text.R
+#' 
+#' Contributors:
+#' 
+#' What this file does:
+#'  - Introduces students to first founding concepts in text analytics
+
+# --- Libraries --- #
+library(readr)     # read files
+library(janitor)   # cleanup var names
+library(dplyr)     # data manip
+library(tibble)    # work with dataframe
+library(tidyr)     # data manip
+library(ggplot2)   # plotting
+library(stringr)   # work with strings
+library(tidytext)  # work with text - main functionality
+library(textstem)  # stem words
+library(reshape2)  # cast from long to wide and vice versa
+library(wordcloud) # plot wordclouds
+
+# --- Download Data --- #
+
+# --- Load Data --- #
+
+# --- Reducing the Data Size --- #
+
+# --- Counting Characters and Approx. Word Counts --- #
+
+# --- Reviews to Tokens --- #
+
+# --- Stop Words --- # 
+
+#--- Custom Stop Words --- #
+
+# --- Stemming --- #
+
+# --- TF-IDF --- #
+
+# --- Word Clouds --- #
